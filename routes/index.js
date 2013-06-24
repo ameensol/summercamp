589e0b40616f3dfd16461f58452bacbd1e3d3e32
Ameen Soleimani <ameen.sol@gmail.com>
2013-06-24 18:30:31 -0400

    test commit on new branch

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+var nodemailer = require("nodemailer");
+var gmailAuth = require('../config.js').gmailAuth
+
 // GET home page
 exports.index = function(req, res){
   res.render('story');
@@ -6,5 +9,29 @@ exports.index = function(req, res){
 // POST home page
 exports.apply = function(req, res){
   console.log('Application Received');
+
+  var smtpTransport = nodemailer.createTransport("SMTP", {
+    service: "Gmail",
+    auth: gmailAuth
+  });
+
+  var mailOptions = {
+    from: "potomaccodecamp@gmail.com",
+    to: "amukunda15@amherst.edu",
+    subject: "Test Email",
+    text: "Did it work?",
+    html: "<b>Hello World</b>"
+  }
+
+  smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){ 
+      console.log(error); 
+    } else {
+      console.log("Message Sent: " + response.message);
+    }
+
+    smtpTransport.close();
+  });
+
   res.render('story');
 };
